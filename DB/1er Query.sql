0f8efefebfe55a9ea9927b9599b1f15b8d2477d3
Vok01 <z3r0o1.vok01@gmail.com>
2015-10-11 14:34:02 -0500

    Se creo un procedimiento almacenado
    Se hizo un Procedimiento almacenado para la insercion de personas,
    revisar

--- DB/1er Query.sql
+++ DB/1er Query.sql
@@ -14,7 +14,7 @@ INSERT INTO Departamento VALUES ('Electronicos', 'Depto. Electronica', 1)
 
 /*Insert en Persona*/
 INSERT INTO Persona VALUES ('Francisco Javier', 'Juan', 'Amador', 'Calle Gavilan #14', '03-04-1990', 1, 'xavier_fjuan@outlook.com', '777-339-69-30', 1, null)
-INSERT INTO Persona VALUES ('Eliel David', 'Rodriguez', null, 'Conocida', '02-08-1992', 1, 'eliel_david@outlook.com', '777-123-45-67', 1, 0)
+INSERT INTO Persona VALUES ('Eliel David', 'Rodriguez', null, 'Conocida', '02-08-1992', 1, 'eliel_david@outlook.com', '777-123-45-67', 2, 0)
 
 /*Insert en Categoria*/
 INSERT INTO Categoria VALUES('Blancos', 1, 1)
@@ -30,3 +30,24 @@ INSERT INTO Venta_detalle VALUES (1, 7000, 1, 1)
 
 /*Select de prueba, no importante*/
 SELECT producto.nombre, cantidad, costo_venta FROM Venta_detalle, Producto WHERE Venta_detalle.idProducto = Producto.idProducto;
+
+/*Primer procedimiento*/
+
+CREATE PROCEDURE InsertaPersona
+	@nombre varchar(20),
+	@app varchar(20),
+	@apm varchar(20),
+	@direccion varchar(50),
+	@fecha_nac date,
+	@estado bit,
+	@correoE varchar(30),
+	@telefono varchar(15),
+	@idUsuario int,
+	@idDepartamento int
+AS
+BEGIN
+	INSERT INTO Usuario VALUES ('alansaucedo','alan123', 4); /*Aqui se va a tomar la primera parte del correo y la contraseña, VERIFICAR ESO*/
+	INSERT INTO Persona VALUES (@nombre,@app,@apm,@direccion,@fecha_nac,@estado,@correoE,@telefono,@idUsuario,@idDepartamento);
+END
+
+EXECUTE InsertaPersona 'Alan', 'Saucedo', 'Colin', 'Conocida', '05-07-1989', 1, 'alansaucedo@outlook.com', '777-320-57-89', 3, 0
